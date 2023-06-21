503f8af1d67bca84c5be00bf849505f17759136a
Mano123 <hermann.agossou@yahoo.fr>
2023-06-21 14:07:04 +0200

    Update 2_Machine Learning.py

--- pages/2_Machine Learning.py
+++ pages/2_Machine Learning.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.write('Entrainement et choix du modèle')
 
-df=pd.read_csv('/content/drive/MyDrive/Data/atp_after_cleaning.csv')
+df=pd.read_csv('atp_after_cleaning.csv')
 df=df.reset_index(drop=True)
 df=df.drop('Unnamed: 0',axis=1)
