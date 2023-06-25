42cb091a8a5a1f870bbe527e9b60ea8fadc03d03
Mano123 <hermann.agossou@yahoo.fr>
2023-06-25 13:34:59 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -208,6 +208,6 @@ df['Ratio_proba']=df.P1_proba_elo/df.P2_proba_elo
 
 df=df.reset_index(drop=True)
 
-df.to_csv('atp_after_cleaning.csv')
+df.to_csv('https://github.com/Mano123/Paris-Sportifs-DA22/blob/main/atp_after_cleaning.csv')
 
 st.write(df)
