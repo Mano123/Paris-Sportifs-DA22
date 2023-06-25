dd6e102ea919d01574a214b0713258fdf2226f04
Mano123 <hermann.agossou@yahoo.fr>
2023-06-25 14:00:00 +0200

    Update 2_Machine Learning.py

--- pages/2_Machine Learning.py
+++ pages/2_Machine Learning.py
@@ -1,7 +1,17 @@
 import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
 
-st.write('Entrainement et choix du modèle')
+st.title('MACHINE LEARNING')
+
+# STATISTIQUES ET VISUALISATIONS
 
 df=pd.read_csv('atp_after_cleaning.csv')
+df=df.drop(['Unnamed: 0','Comment'],axis=1)
+df.Date=pd.to_datetime(df.Date)
 df=df.reset_index(drop=True)
-df=df.drop('Unnamed: 0',axis=1)
+df['Result']=df['Result'].astype('str')
