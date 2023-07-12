168c7c876154db108fcf7bce8fa7b98f0f2047d9
Mano123 <hermann.agossou@yahoo.fr>
2023-07-12 18:30:15 +0200

    Update 2_Machine Learning.py

--- pages/2_Machine Learning.py
+++ pages/2_Machine Learning.py
@@ -152,9 +152,7 @@ if uploaded_file is not None:
         df_pred.loc[row,'P1_B365']=df_pred.loc[row,'P2_B365']
         df_pred.loc[row,'P2_B365']=var_echange
     
-    modify = st.checkbox("Add filters")
 
-    df_pred = df_pred.copy()
 
     modification_container = st.container()
 
@@ -170,7 +168,7 @@ if uploaded_file is not None:
                     df_pred[column].unique(),
                     default=list(df_pred[column].unique()),
                 )
-                df_pred = df_pred[df_pred[column].isin(user_cat_input)]
+                #df_pred = df_pred[df_pred[column].isin(user_cat_input)]
     
     st.write(df_pred)
     
