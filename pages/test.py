930b7811e0e8cea0eaaa03cec17e0549e99fca69
Mano123 <hermann.agossou@yahoo.fr>
2023-07-12 13:52:13 +0200

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -50,7 +50,7 @@ def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
     with modification_container:
         to_filter_columns = st.multiselect("Filter dataframe on", df.columns)
         for column in to_filter_columns:
-            #left, right = st.columns((1, 20))
+            left, right = st.columns((1, 20))
             #left.write("↳")
             
     return df
