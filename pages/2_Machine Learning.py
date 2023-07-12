e605ac2765ad5c23ee7e4f38c7cd3b75cfd14916
Mano123 <hermann.agossou@yahoo.fr>
2023-07-12 22:53:21 +0200

    Update 2_Machine Learning.py

--- pages/2_Machine Learning.py
+++ pages/2_Machine Learning.py
@@ -162,8 +162,9 @@ if uploaded_file is not None:
         serie = st.selectbox("Series", df_pred[df_pred.Location==location].Series.unique())
         court = st.selectbox("Court", df_pred[df_pred.Location==location].Court.unique())
         round = st.selectbox("Round", df_pred[df_pred.Location==location].Round.unique())
-        player1 = st.selectbox("Player 1", df_pred[df_pred.Location==location & df_pred.Round==round].Player1.unique())
-        player2 = st.selectbox("Player 2", df_pred[df_pred.Location==location & df_pred.Round==round].Player2.unique())
+        #player1 = st.selectbox("Player 1", df_pred[df_pred.Location==location & df_pred.Round==round].Player1.unique())
+        #player2 = st.selectbox("Player 2", df_pred[df_pred.Location==location & df_pred.Round==round].Player2.unique())
+        df_pred[df_pred.Location==location & df_pred.Round==round].Player1.unique())
             
             
 
