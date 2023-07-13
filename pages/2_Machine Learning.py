dc04c5a50558b6ea361d2999430fb9aafef41a10
Mano123 <hermann.agossou@yahoo.fr>
2023-07-13 02:03:52 +0200

    Update 2_Machine Learning.py

--- pages/2_Machine Learning.py
+++ pages/2_Machine Learning.py
@@ -176,6 +176,9 @@ if uploaded_file is not None:
         st.write('Joueur 2 : '+player2)
         st.write('Niveau du Tournoi : '+round)
 
-        st.write(df_filtre.Result.values)
+        if df_filtre.Result.values==1:
+            st.write('Le joueur '+player1+' a plus de chance de gagner ce match par rapport au joueur '+player2)
+        else:
+            st.write('Le joueur '+player2+' a plus de chance de gagner ce match par rapport au joueur '+player1)
     
         
