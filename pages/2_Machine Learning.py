4d5d6050f9ecd236070dc4b47a558d1f2f9a16b2
Mano123 <hermann.agossou@yahoo.fr>
2023-07-14 09:33:40 +0200

    Update 2_Machine Learning.py

--- pages/2_Machine Learning.py
+++ pages/2_Machine Learning.py
@@ -152,6 +152,7 @@ if uploaded_file is not None:
             
         df_filtre=df_pred[(df_pred.Location==location) & (df_pred.Tournament==tournament) & (df_pred.Series==serie) & (df_pred.Court==court) & (df_pred.Round==round) & (df_pred.Player1==player1) & (df_pred.Player2==player2)]    
 
+        capital_depart = st.slider('Capital de départ', 0, 10000, 50,50)
         st.write('Tournoi de '+tournament)
         st.write('Date du tournoi : '+date.strftime('%d-%m-%Y'))
         st.write('Joueur 1 : '+player1)
