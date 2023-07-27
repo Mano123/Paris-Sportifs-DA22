2a7c2172612b3e0477f94ecd9889287da17d5652
Mano123 <hermann.agossou@yahoo.fr>
2023-07-27 19:41:46 +0200

    Update 2_Machine Learning.py

--- pages/2_Machine Learning.py
+++ pages/2_Machine Learning.py
@@ -140,21 +140,6 @@ y_pred=forest.predict(df_test_scaled)
 df_pred=pd.concat([df_test,pd.Series(y_pred),pd.DataFrame(y_pred_proba,columns=['Proba 0','Proba 1'])['Proba 0'],pd.DataFrame(y_pred_proba,columns=['Proba 0','Proba 1'])['Proba 1']],axis=1)
 df_pred=df_pred.rename(columns={0:'Result',1:'Proba 0',2:'Proba 1'})
 
-for row in df_pred.index:
-    if df_pred.loc[row,'Result']==0:
-        var_inter=df_pred.loc[row,'Player1']
-        df_pred.loc[row,'Player1']=df_pred.loc[row,'Player2']
-        df_pred.loc[row,'Player2']=var_inter
-        var_inter=df_pred.loc[row,'P1Rank']
-        df_pred.loc[row,'P1Rank']=df_pred.loc[row,'P2Rank']
-        df_pred.loc[row,'P2Rank']=var_inter
-        var_inter=df_pred.loc[row,'P1_PS']
-        df_pred.loc[row,'P1_PS']=df_pred.loc[row,'P2_PS']
-        df_pred.loc[row,'P2_PS']=var_inter
-        var_inter=df_pred.loc[row,'P1_B365']
-        df_pred.loc[row,'P1_B365']=df_pred.loc[row,'P2_B365']
-        df_pred.loc[row,'P2_B365']=var_inter
-
 modification_container = st.container()
 
 with modification_container:
