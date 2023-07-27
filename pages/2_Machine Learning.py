62065c0bd62af146d60b4a9956d76394226d8795
Mano123 <hermann.agossou@yahoo.fr>
2023-07-27 18:54:24 +0200

    Update 2_Machine Learning.py

--- pages/2_Machine Learning.py
+++ pages/2_Machine Learning.py
@@ -140,7 +140,8 @@ y_pred=forest.predict(df_test_scaled)
 df_pred=pd.concat([df_test,pd.Series(y_pred),pd.DataFrame(y_pred_proba,columns=['Proba 0','Proba 1'])['Proba 0'],pd.DataFrame(y_pred_proba,columns=['Proba 0','Proba 1'])['Proba 1']],axis=1)
 df_pred=df_pred.rename(columns={0:'Result',1:'Proba 0',2:'Proba 1'})
 
-if df_pred.loc[row,'Result']==0:
+for row in df_pred.index:
+    if df_pred.loc[row,'Result']==0:
     var_inter=df_pred.loc[row,'Player1']
     df_pred.loc[row,'Player1']=df_pred.loc[row,'Player2']
     df_pred.loc[row,'Player2']=var_inter
