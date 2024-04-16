ed80c09678ca69d5b2ce08c31e9bfb9f50f0b5df
Hermann AGOSSOU <hermann.agossou@yahoo.fr>
2024-04-16 09:04:29 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -11,5 +11,5 @@ st.set_page_config(
 
 st.title("PROJET PARIS SPORTIFS")
 
-st.text('Ce projet porte sur l'\ATP Tour anciennement connu sous le nom d'ATP World Tour entre 2009 et 2018) est le principal circuit international de tennis masculin. Son équivalent féminin est le WTA Tour. 
-Organisé par l'Association of Tennis Professionals, il a été créé en 1990 en remplacement du Grand Prix tennis circuit. Le circuit secondaire est l'ATP Challenger Tour et le troisième niveau est le circuit masculin de l'ITF.)
+st.text("Ce projet porte sur l'ATP Tour anciennement connu sous le nom d'ATP World Tour entre 2009 et 2018) est le principal circuit international de tennis masculin. Son équivalent féminin est le WTA Tour. 
+Organisé par l'Association of Tennis Professionals, il a été créé en 1990 en remplacement du Grand Prix tennis circuit. Le circuit secondaire est l'ATP Challenger Tour et le troisième niveau est le circuit masculin de l'ITF.")
